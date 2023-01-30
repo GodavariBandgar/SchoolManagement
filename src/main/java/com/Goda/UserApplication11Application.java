6834bf7249ef22cc37ec6d43ce35e9b738cfcbaf
Godavari Bandgar <bandgar 101@gmail.com>
2023-01-31 00:33:27 +0530

    Goda project

---
+++ src/main/java/com/Goda/UserApplication11Application.java
@@ -0,0 +1,13 @@
+package com.Goda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Goda/UserApplication11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Goda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplication11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
